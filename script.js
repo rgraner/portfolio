aaaef5c1a1cf28efda617d6fb6e551326013ca17
rgraner <ricardograner@gmail.com>
2022-12-13 11:52:16 +0000

    Create dark mode button

---
+++ script.js
@@ -0,0 +1,28 @@
+const darkModeToggle = document.getElementById("dark-mode");
+const lightModeToggle = document.getElementById("light-mode");
+let darkNavItem = document.getElementsByTagName("a");
+
+function darkMode() {
+    document.body.style.backgroundColor = "rgb(32, 32, 32)";
+    document.body.style.color = "white";
+    darkModeToggle.style.display = "none";
+    lightModeToggle.style.display = "block";
+    // lightModeToggle.style.color= "#222";
+    // lightModeToggle.style.backgroundColor = "white";
+    for (let i = 0; i < darkNavItem.length; i++) {
+        darkNavItem[i].style.color = "white";
+    }
+}
+
+function lightMode() {
+    document.body.style.backgroundColor = "";
+    document.body.style.color = "";
+    darkModeToggle.style.display = "";
+    lightModeToggle.style.display = "";
+    for (let i = 0; i < darkNavItem.length; i++) {
+        darkNavItem[i].style.color = "";
+    } 
+}
+
+lightModeToggle.onclick = lightMode;
+darkModeToggle.onclick = darkMode;  
