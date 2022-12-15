95b595d253c51c903074bda4a1c0c401d6ff0763
rgraner <ricardograner@gmail.com>
2022-12-15 18:43:41 +0000

    Add position fixed navbar

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const darkModeToggle = document.getElementById("dark-mode");
 const lightModeToggle = document.getElementById("light-mode");
 const darkNavItem = document.getElementsByTagName("a");
+const darkNavBg = document.querySelector("header");
+console.log(darkNavBg);
 
 function darkMode() {
     document.body.style.backgroundColor = "rgb(39, 39, 39)";
     document.body.style.color = "white";
     darkModeToggle.style.display = "none";
     lightModeToggle.style.display = "block";
+    darkNavBg.style.backgroundColor = "rgb(39, 39, 39)";
     // lightModeToggle.style.color= "#222";
     // lightModeToggle.style.backgroundColor = "white";
     for (let i = 0; i < darkNavItem.length; i++) {
@@ -20,6 +23,7 @@ function lightMode() {
     document.body.style.color = "";
     darkModeToggle.style.display = "";
     lightModeToggle.style.display = "";
+    darkNavBg.style.backgroundColor = "";
     for (let i = 0; i < darkNavItem.length; i++) {
         darkNavItem[i].style.color = "";
     } 
